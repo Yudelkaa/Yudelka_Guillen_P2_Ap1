a0dd511c7283d56365f4c72b34864cb7de4f4578
Yudelka <yudelkaguillen07@gmail.com>
2024-03-04 18:48:03 -0400

    Revert "Modelos y validaciones"
    This reverts commit 1ec82847423e1af0e715bd70bd6a59cc276d1807.

--- Yudelka_Guillen_P2_Ap1.Api/DAL/Contexto.cs
+++ Yudelka_Guillen_P2_Ap1.Api/DAL/Contexto.cs
@@ -1,17 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Shared.Models;
-using System.Reflection.Emit;
-using Yudelka_Guillen_P2_Ap1.Api.DAL;
 
 namespace Yudelka_Guillen_P2_Ap1.Api.DAL;
 
 public class Contexto : DbContext
 {
-	public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-	public DbSet<Vehiculo> Tickets { get; set; }
-	public DbSet<VehiculoDetalle> TicketsDetalles { get; set; }
+
 }
-	
-	
 
