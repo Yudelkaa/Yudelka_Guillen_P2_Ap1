1ec82847423e1af0e715bd70bd6a59cc276d1807
Yudelka <yudelkaguillen07@gmail.com>
2024-03-04 18:47:18 -0400

    Modelos y validaciones

---
+++ Shared/Models/Vehiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class Vehiculo
+	{
+		public int VehiculoId { get; set; }
+		[Required(ErrorMessage = "Es requerido")]
+		public string? Descripcion { get; set; }
+		[Required(ErrorMessage = "Es requerido")]
+		public DateTime Fecha { get; set; } = DateTime.Now;
+		[Required(ErrorMessage = "Es requerido")]
+		public double Costo { get; set; }
+		[Required(ErrorMessage = "Es requerido")]
+		public double Gastos { get; set; }
+
+		[ForeignKey("VehiculoId")]
+		public ICollection<VehiculoDetalle> VehiculoDetalle { get; set; } = new List<VehiculoDetalle>();
+	}
+
+}
--- Yudelka_Guillen_P2_Ap1.Api/DAL/Contexto.cs
+++ Yudelka_Guillen_P2_Ap1.Api/DAL/Contexto.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Shared.Models;
+using System.Reflection.Emit;
+using Yudelka_Guillen_P2_Ap1.Api.DAL;
 
 namespace Yudelka_Guillen_P2_Ap1.Api.DAL;
 
 public class Contexto : DbContext
 {
-
+	public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+	public DbSet<Vehiculo> Tickets { get; set; }
+	public DbSet<VehiculoDetalle> TicketsDetalles { get; set; }
 }
+	
+	
 
