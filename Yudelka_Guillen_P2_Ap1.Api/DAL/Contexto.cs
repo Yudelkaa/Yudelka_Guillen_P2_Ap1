5bce120595cc236e50b0aacc73937b8107b28c34
Yudelka <yudelkaguillen07@gmail.com>
2024-03-04 18:54:05 -0400

    Modelos y validaciones

--- Yudelka_Guillen_P2_Ap1.Api/DAL/Contexto.cs
+++ Yudelka_Guillen_P2_Ap1.Api/DAL/Contexto.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
 namespace Yudelka_Guillen_P2_Ap1.Api.DAL;
 
 public class Contexto : DbContext
 {
+	public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+	public DbSet<Vehiculo> Vehiculo { get; set; }
+	public DbSet<VehiculoDetalle> VehiculoDetalle { get; set; }
 
 }
-
